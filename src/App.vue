396c557d76e15f5c2c96ed0f399af41ff7a2ea8f
pan <pz886688@163.com>
2021-06-30 01:48:09 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    APP组件的
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false; //浏览器不显示非生产环境打包的提示
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
